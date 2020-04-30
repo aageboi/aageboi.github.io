86f62ff8b78d6ecac5f153e1d3d3557098f0ffeb
Heri Gunawan <heri@tirto.id>
2020-04-30 14:21:45 +0700

    [ADD] react-furniture-search build

---
+++ react-furniture-search/precache-manifest.d85b218714a5c69b1d31668a0126c3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec156fffd96815b485965414b7a3ee7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5077bf0b66c18c08a55",
+    "url": "/static/css/main.30bdb988.chunk.css"
+  },
+  {
+    "revision": "c7dd2b797811c3321606",
+    "url": "/static/js/2.1fdfb067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fdfb067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5077bf0b66c18c08a55",
+    "url": "/static/js/main.3ea824bc.chunk.js"
+  },
+  {
+    "revision": "52228a6739d1122f02e9",
+    "url": "/static/js/runtime-main.d500496a.js"
+  }
+]);
