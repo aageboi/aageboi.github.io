0518e9633d88ac66b695263900ee6640a6a3dde6
Heri Gunawan <heri@tirto.id>
2020-05-31 20:12:33 +0700

    [UPDATE] base url

--- quiz-app/end.js
+++ quiz-app/end.js
@@ -25,5 +25,5 @@ saveHighScore = e => {
   highScores.splice(MAX_HIGH_SCORES);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
+  window.location.assign("https://aageboi.github.io/quiz-app/");
 };
